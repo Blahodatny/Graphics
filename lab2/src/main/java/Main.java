d175697573fe547c9ed19244cf58d9cc38d70001
Blahodatny <dbrlmola82@gmail.com>
2019-03-18 00:32:01 +0200

    window

--- lab2/src/main/java/Main.java
+++ lab2/src/main/java/Main.java
@@ -1,2 +1,37 @@
-public class Main {
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+class Main extends JPanel {
+    private final static int FRAME_WIDTH = 1500;
+    private final static int FRAME_HEIGHT = 1000;
+    private static int maxWidth;
+    private static int maxHeight;
+
+    public void paint(Graphics graphics) {
+        var g2d = (Graphics2D) graphics;
+        var rh = new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON
+        );
+        rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHints(rh);
+    }
+
+    public static void main(String... args) {
+        var frame = new JFrame("Lab 2");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.add(new Main());
+        frame.setVisible(true);
+
+        var size = frame.getSize();
+        var insets = frame.getInsets();
+        maxWidth = size.width - insets.left - insets.right - 1;
+        maxHeight = size.height - insets.top - insets.bottom - 1;
+    }
 }
